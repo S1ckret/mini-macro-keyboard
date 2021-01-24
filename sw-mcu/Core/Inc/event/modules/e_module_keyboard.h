4e76739ba7942a9b9c2603617392cff58411cfab
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-01-24 12:50:44 +0200

    feat(sw-mcu): add keyboard module
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ sw-mcu/Core/Inc/event/modules/e_module_keyboard.h
@@ -0,0 +1,43 @@
+/*
+ * e_module_keyboard.h
+ *
+ *  Created on: Jan 24, 2021
+ *      Author: S1ckret
+ */
+
+#ifndef INC_EVENT_MODULES_E_MODULE_KEYBOARD_H_
+#define INC_EVENT_MODULES_E_MODULE_KEYBOARD_H_
+
+#include "event/modules/e_module.h"
+
+enum e_module_key {
+  MOD_KEY_1 = 0u,
+  MOD_KEY_2,
+  MOD_KEY_3,
+  MOD_KEY_COUNT
+};
+
+enum e_module_layout {
+  MOD_LAYOUT_A = 0u,
+  MOD_LAYOUT_B,
+  MOD_LAYOUT_COUNT
+};
+
+struct e_module_key_data {
+  uint8_t modifiers;
+  uint8_t key;
+};
+
+struct e_module_keyboard {
+  struct e_module super;
+  struct e_module_key_data
+    keys[MOD_LAYOUT_COUNT][MOD_KEY_COUNT];
+  enum e_module_layout current_layout;
+};
+
+void e_module_keyboard_ctor(struct e_module_keyboard *me,
+                              uint8_t *name);
+
+void e_module_keyboard_dtor(struct e_module_keyboard *me);
+
+#endif /* INC_EVENT_MODULES_E_MODULE_KEYBOARD_H_ */
---
+++ sw-mcu/Core/Src/event/modules/e_module_keyboard.c
@@ -0,0 +1,41 @@
+/*
+ * e_module_keyboard.c
+ *
+ *  Created on: Jan 24, 2021
+ *      Author: S1ckret
+ */
+
+#include "event/modules/e_module.h"
+#include "event/modules/e_module_keyboard.h"
+
+#include "drv/drv_led.h"
+
+static void keyboard_dispatch(struct e_module *me, struct e_event *e);
+
+void e_module_keyboard_ctor(struct e_module_keyboard *me,
+                              uint8_t *name) {
+  e_module_ctor((struct e_module *)me, name, keyboard_dispatch);
+  me->current_layout = MOD_LAYOUT_A;
+}
+
+void e_module_keyboard_dtor(struct e_module_keyboard *me) {
+  e_module_dtor((struct e_module *)me);
+}
+
+static void keyboard_dispatch(struct e_module *me, struct e_event *e) {
+  if (me != e->mod_to) return;
+
+  struct e_module_keyboard *keyboard = (struct e_module_keyboard *)me;
+  switch(e->sig) {
+  case SIG_SYS_INIT:
+    break;
+  case SIG_LAYOUT_OFF:
+    keyboard->current_layout = MOD_LAYOUT_A;
+    break;
+  case SIG_LAYOUT_ON:
+    keyboard->current_layout = MOD_LAYOUT_B;
+    break;
+  default:
+    break;
+  }
+}
