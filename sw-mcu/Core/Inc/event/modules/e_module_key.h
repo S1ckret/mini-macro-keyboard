c6d43b77e9373a318b75ba142cedb8d917806a4e
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-02-03 19:28:39 +0200

    style(sw-mcu): rename drv_key to drv_switch
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- sw-mcu/Core/Inc/event/modules/e_module_key.h
+++ sw-mcu/Core/Inc/event/modules/e_module_key.h
@@ -8,11 +8,11 @@
 #ifndef INC_EVENT_MODULES_E_MODULE_KEY_H_
 #define INC_EVENT_MODULES_E_MODULE_KEY_H_
 
+#include "drv/drv_switch.h"
 #include "event/modules/e_module.h"
 
 #include "event/e_timer.h"
 
-#include "drv/drv_key.h"
 
 enum e_module_key_state {
   MOD_KEY_STATE_PRESSED = 0u,
@@ -21,14 +21,14 @@ enum e_module_key_state {
 
 struct e_module_key {
   struct e_module super;
-  enum drv_key_name key;
+  enum drv_switch_name key;
   enum e_module_key_state key_state;
   struct e_timer timer;
 };
 
 void e_module_key_ctor(struct e_module_key *me,
                               uint8_t *name,
-                              enum drv_key_name key);
+                              enum drv_switch_name key);
 
 void e_module_key_dtor(struct e_module_key *me);
 
