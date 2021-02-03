6f9eb2463136df41f185f3568dc8d2f93a3ad58e
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-02-03 21:08:36 +0200

    feat(sw-mcu): create flexible key module
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- sw-mcu/Core/Src/event/modules/e_module_key.c
+++ sw-mcu/Core/Src/event/modules/e_module_key.c
@@ -14,32 +14,46 @@
 #include "event/events/e_event.h"
 #include "event/events/e_event_key.h"
 
-
 #define KEY_DEBAUNCE_TIME 20
 
 static void key_dispatch(struct e_module *me, struct e_event *e);
 
 void e_module_key_ctor(struct e_module_key *me,
-                              uint8_t *name,
-                              enum drv_switch_name key) {
+                          uint8_t *name,
+                          enum drv_switch_name key,
+                          enum e_signal sig_on_press,
+                          enum e_signal sig_on_release,
+                          struct e_module *mod_to) {
   e_module_ctor((struct e_module *)me, name, key_dispatch);
   me->key = key;
   me->key_state = MOD_KEY_STATE_RELEASED;
+  me->sig_on_press = sig_on_press;
+  me->sig_on_release = sig_on_release;
+  me->mod_to = mod_to;
 }
 
 void e_module_key_dtor(struct e_module_key *me) {
   e_module_dtor((struct e_module *)me);
 }
 
-static void form_event(struct e_module *me,
-                        struct e_event_key *e,
-                        enum e_signal sig,
-                        enum drv_switch_name key) {
+static void form_event(struct e_module_key *me,
+                        struct e_event_key *e) {
   e->super.mod_from = me;
-  e->super.mod_to = e_pmod_keyboard;
-  e->super.sig = sig;
+  e->super.mod_to = me->mod_to;
   e->super.size = 1;
-  e->key = key;
+  e->key = me->key;
+}
+
+static void form_event_on_press(struct e_module_key *me,
+                        struct e_event_key *e) {
+  form_event(me, e);
+  e->super.sig = me->sig_on_press;
+}
+
+static void form_event_on_release(struct e_module_key *me,
+                        struct e_event_key *e) {
+  form_event(me, e);
+  e->super.sig = me->sig_on_release;
 }
 
 static void key_dispatch(struct e_module *me, struct e_event *e) {
@@ -70,7 +84,7 @@ static void key_dispatch(struct e_module *me, struct e_event *e) {
         me_key->key_state = MOD_KEY_STATE_RELEASED;
         /* Generate event to other modules */
         struct e_event_key event_to_send;
-        form_event(me, &event_to_send, SIG_KEY_PRESSED, me_key->key);
+        form_event_on_press(me, &event_to_send);
         e_core_notify((struct e_event *)(&event_to_send));
       }
       /* It was bounce */
@@ -81,7 +95,7 @@ static void key_dispatch(struct e_module *me, struct e_event *e) {
         me_key->key_state = MOD_KEY_STATE_PRESSED;
         /* Generate event to other modules */
         struct e_event_key event_to_send;
-        form_event(me, &event_to_send, SIG_KEY_RELEASED, me_key->key);
+        form_event_on_release(me, &event_to_send);
         e_core_notify((struct e_event *)(&event_to_send));
       }
       /* It was bounce */
