e6473171d8418ff7d4c1dcc754acb111b4d98d83
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-02-03 21:08:36 +0200

    feat(sw-mcu): create flexible key module
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- sw-mcu/Core/Inc/event/modules/e_module_key.h
+++ sw-mcu/Core/Inc/event/modules/e_module_key.h
@@ -9,6 +9,8 @@
 #define INC_EVENT_MODULES_E_MODULE_KEY_H_
 
 #include "drv/drv_switch.h"
+
+#include "event/e_signals.h"
 #include "event/modules/e_module.h"
 
 #include "event/e_timer.h"
@@ -24,11 +26,17 @@ struct e_module_key {
   enum drv_switch_name key;
   enum e_module_key_state key_state;
   struct e_timer timer;
+  enum e_signal sig_on_press;
+  enum e_signal sig_on_release;
+  struct e_module *mod_to;
 };
 
 void e_module_key_ctor(struct e_module_key *me,
-                              uint8_t *name,
-                              enum drv_switch_name key);
+                          uint8_t *name,
+                          enum drv_switch_name key,
+                          enum e_signal sig_on_press,
+                          enum e_signal sig_on_release,
+                          struct e_module *mod_to);
 
 void e_module_key_dtor(struct e_module_key *me);
 
