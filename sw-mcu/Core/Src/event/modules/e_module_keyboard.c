52c6ea9be4869d3eea2a3bf0444844e81ab59b07
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-02-06 18:06:44 +0200

    feat(sw-mcu): add led toggle on key press
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- sw-mcu/Core/Src/event/modules/e_module_keyboard.c
+++ sw-mcu/Core/Src/event/modules/e_module_keyboard.c
@@ -40,6 +40,7 @@ static void keyboard_dispatch(struct e_module *me, struct e_event *e) {
   case SIG_KEY_PRESSED:
   {
     struct e_event_key *e_key = (struct e_event_key *)e;
+    drv_led_toggle(e_key->key + 1);
     /* Send info to the hid codec module */
     struct e_module_key_data *pressed_key = &(keyboard->keys[keyboard->current_layout][e_key->key]);
   }
@@ -47,6 +48,7 @@ static void keyboard_dispatch(struct e_module *me, struct e_event *e) {
   case SIG_KEY_RELEASED:
   {
     struct e_event_key *e_key = (struct e_event_key *)e;
+    drv_led_toggle(e_key->key + 1);
     /* Send info to the hid codec module */
     struct e_module_key_data *pressed_key = &(keyboard->keys[keyboard->current_layout][e_key->key]);
   }
