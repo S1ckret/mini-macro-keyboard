b2fe46d4c6f3b9cb98ea0fe96a3bf29526488571
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-02-12 04:09:19 +0200

    feat(hid-sniffer): add report read/write
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- sw-pc/hid-sniffer/src/main.c
+++ sw-pc/hid-sniffer/src/main.c
@@ -7,30 +7,31 @@
 #define KEYBOARD_PRODUCT_ID (0x5750u)
 
 void print_hid_info(struct hid_device_info *dev);
+void print_report_content(unsigned char *report, unsigned report_size);
 
 struct hid_device_info *find_keyboard(struct hid_device_info *devs);
 
 int main(int argc, char* argv[])
 {
 	if (hid_init()) return -1;
+	printf("HID API has been inited...\n");
 
-	struct hid_device_info *devs = hid_enumerate(0x0, 0x0);
-
-	struct hid_device_info *keyboard = find_keyboard(devs);
-
-	if (keyboard) {
-		print_hid_info(keyboard);
-		printf("The mini-macro-keyboard was found!\n");
-	}
-	else {
+	hid_device *keyboard = hid_open(KEYBOARD_VENDOR_ID, KEYBOARD_PRODUCT_ID, NULL);
+	if (!keyboard) {
 		printf("The mini-macro-keyboard wasn't found! Please try again.\n");
 	}
+	printf("The mini-macro-keyboard was found!\n");
+	unsigned char report_out[8] = {2, 69, 0, 0, 0, 0, 0, 0};
+	printf("Trying to send output report...\n");
+	print_report_content(report_out, 2);
+	hid_write(keyboard, report_out, 2);
 
-	hid_free_enumeration(devs);
+	printf("Trying to get intput report...\n");
+	unsigned char report_in[8] = {0};
+	hid_read_timeout(keyboard, report_in, 2, 2000);
+	print_report_content(report_in, 2);
 
-#ifdef WIN32
-	system("pause");
-#endif
+	hid_close(keyboard);
 
 	return 0;
 }
@@ -40,6 +41,15 @@ void print_hid_info(struct hid_device_info *dev) {
 	printf("  PID: 0x%04hx\n", dev->product_id);
 	printf("  serial_number: %ls", dev->serial_number);
 	printf("  Manufacturer: %ls\n", dev->manufacturer_string);
+}
+
+void print_report_content(unsigned char *report, unsigned report_size) {
+	printf("Report content: [");
+	printf("%03d", report[0]);
+	for (unsigned i = 1U; i < report_size; ++i) {
+		printf(", %03d", report[i]);
+	}
+	printf("]\n");
 }
 
 struct hid_device_info *find_keyboard(struct hid_device_info *devs) {
