1b4969fc9a6550d3bd505e81e9996351bdc5d4d2
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-02-15 21:48:47 +0200

    feat(sw-mcu): handle SIG_KEY_PRESSED / RELEASED in report codec module
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- sw-mcu/Core/Src/event/modules/e_module_report_codec.c
+++ sw-mcu/Core/Src/event/modules/e_module_report_codec.c
@@ -11,6 +11,10 @@
 #include "event/events/e_event_report.h"
 #include "event/events/e_event_key_data.h"
 
+#include "usbd_custom_hid_if.h"
+
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
 static void report_codec_dispatch(struct e_module *me, struct e_event *e);
 
 void e_module_report_codec_ctor(struct e_module_report_codec *me,
@@ -28,6 +32,36 @@ static void report_codec_dispatch(struct e_module *me, struct e_event *e) {
   struct e_module_report_codec *report_codec = (struct e_module_report_codec *)me;
   switch(e->sig) {
   case SIG_SYS_INIT:
+    break;
+    /* Supports only single macro now */
+  case SIG_KEY_PRESSED:
+  {
+    struct e_event_key_data *e_key_data = (struct e_event_key_data *)e;
+    report_codec->current_key_data = e_key_data->p_key_data;
+    if (report_codec->current_key_data->mode == MOD_KEY_MODE_MARCO_SEQ) {
+      report_codec->report_data_in[0] = 2U;
+      report_codec->report_data_in[1] = report_codec->current_key_data->modifiers;
+      report_codec->report_data_in[2] = 0U; /* Reserved */
+      report_codec->report_data_in[3] = report_codec->current_key_data->key;
+
+      USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, report_codec->report_data_in, 9);
+    }
+
+  }
+    break;
+  case SIG_KEY_RELEASED:
+    report_codec->report_data_in[0] = 0U;
+    report_codec->report_data_in[1] = 0U;
+    report_codec->report_data_in[2] = 0U;
+    report_codec->report_data_in[3] = 0U;
+    report_codec->report_data_in[4] = 0U;
+    report_codec->report_data_in[5] = 0U;
+    report_codec->report_data_in[6] = 0U;
+    report_codec->report_data_in[7] = 0U;
+    report_codec->report_data_in[8] = 0U;
+
+    USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, report_codec->report_data_in, 9);
+
     break;
   default:
     break;
