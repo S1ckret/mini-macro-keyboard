474be7b7fbd161f7c392269dcd5977118247fbf3
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-02-20 10:12:17 +0200

    feat(hid-sniffer): add create macro report
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- sw-pc/hid-sniffer/src/main.c
+++ sw-pc/hid-sniffer/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "hidapi.h"
 
 #define KEYBOARD_VENDOR_ID (0x0483u)
@@ -10,6 +11,13 @@ void print_hid_info(struct hid_device_info *dev);
 void print_report_content(unsigned char *report, unsigned report_size);
 
 struct hid_device_info *find_keyboard(struct hid_device_info *devs);
+
+struct report_out_create_macro {
+	uint8_t report_id;
+	uint8_t key_address;
+	uint8_t dynamic_flags;
+	uint8_t key[6];
+};
 
 int main(int argc, char* argv[])
 {
@@ -21,10 +29,20 @@ int main(int argc, char* argv[])
 		printf("The mini-macro-keyboard wasn't found! Please try again.\n");
 	}
 	printf("The mini-macro-keyboard was found!\n");
-	unsigned char report_out[8] = {2, 69, 0, 0, 0, 0, 0, 0};
+	struct report_out_create_macro report_out;
+	report_out.report_id = 77;
+	report_out.key_address = 1; /*button 2*/
+	report_out.dynamic_flags = 0;
+	report_out.key[0] = 8; /* E key */
+	report_out.key[1] = 0;
+	report_out.key[2] = 0;
+	report_out.key[3] = 0;
+	report_out.key[4] = 0;
+	report_out.key[5] = 0;
 	printf("Trying to send output report...\n");
-	print_report_content(report_out, 2);
-	hid_write(keyboard, report_out, 2);
+	print_report_content((uint8_t *) &report_out, 9);
+	hid_write(keyboard, (uint8_t *) &report_out, 9);
+
 
 	printf("Trying to get intput report...\n");
 	unsigned char report_in[8] = {0};
